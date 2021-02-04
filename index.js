7bb849679df2aeb4a90fe01b8f078b5ece5ce3a3
Lucasbr2k14 <46508925+Lucasbr2k14@users.noreply.github.com>
2021-02-04 05:07:01 -0300

    funcional

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 function Calcular(){
     /*
     Pegando os elementos 
-    origem
-    destino
+    DDD
     plano
     minutos
     */
-
-
-   
-   const origem = document.getElementById("origem").value
-   const destino = document.getElementById("destino").value
+   const ddd = document.getElementById("DDD").value
    const plano = document.getElementById("plano").value
    const min = document.getElementById("min").value
 
-    if(origem == "Origem" || destino == "Destino" || plano == "Planos" || min == ""){
-        window.alert("Invalido")
+    /*
+    Pegando os campos de escrita
+    Com o valor sem o plano
+    */
+    const tela = {
+        comPlano: document.getElementById("valor"),
+        semPlano: document.getElementById("valorS")
+
+    }
+
+    /* Trasformando os valores em tipo numérico*/
+    const Min = parseInt(min)
+    const DDD = parseFloat(ddd)
+
+    /* Variaveis que vazam o escopo para os calculos*/
+    let comPlano = null
+    let valor = null
+    let valorMin = null 
+    let porcentagem = null
+
+    /*Verificando os se os valores estão completos*/
+    if(ddd == "Origem | Destino" || plano == "Planos" || min == ""){
+        window.alert("Ei, há alguns valores faltando")
+
+    }else{
+
+        valor = DDD * Min
+
+        if(Min <= plano){
+            comPlano = 0
+            console.log("Menor igual")
+        }else{
+            console.log("Maior")
+
+            valorMin = (Min - plano) * DDD
+            porcentagem = (valorMin * 10)/100
+            comPlano = valorMin + porcentagem
+
+        }
+
+
+        /*Escrevendo valores em html*/
+        tela.semPlano.innerHTML = `Valor sem o plano: ${valor}R$`
+        tela.comPlano.innerHTML = `Valor com o plano: ${comPlano}R$`
     }
    
 }
